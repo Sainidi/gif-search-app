cbe2ff4a7071e3c057ae8a4d264695bf3a725e18
Sainidi <Saynidi96@gmail.com>
2021-11-17 17:10:51 +0200

    create app

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ...require('prettier-airbnb-config'),
+    printWidth: 120,
+    proseWrap: 'always',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: true,
+};
